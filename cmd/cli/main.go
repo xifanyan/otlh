3b0b914e3d6100ee213179f3ef1f818469dfcc69
Paul Yan <pyan@opentext.com>
2024-08-12 13:29:34 -0400

    v0.3.6-beta

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	app := &cli.App{
 		Name:    "otlh",
-		Version: "0.3.5-beta",
+		Version: "0.3.6-beta",
 		Usage:   "Command Line Interface to access Opentext LegalHold service",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
