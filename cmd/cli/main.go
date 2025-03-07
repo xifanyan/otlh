428fdc02abbc706cb5270823831e3da0c7c0c2ec
Paul Yan <xifan.yan@gmail.com>
2025-03-07 13:38:43 -0800

    update version

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	app := &cli.App{
 		Name:    "otlh",
-		Version: "0.5.4-beta",
+		Version: "0.5.5-beta",
 		Usage:   "Command Line Interface to access Opentext LegalHold service",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
